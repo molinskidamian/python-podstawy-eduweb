72266f12dc4d4ef5eda324470e945a7cdbe7be6e
Damian Molinski <tedeer@gmail.com>
2021-09-12 20:52:56 +0200

    Begin chapter 8 - petle

---
+++ main.py
@@ -0,0 +1,8 @@
+ski_jumpers = [
+    {"Ryoyu Kobayashi": "JPN"},
+    {'Kamil Stoch', "PL"},
+    {"Stefan Kraft", "AU"},
+    {"Robert Johansson", "NOR"},
+    {"Piotr Żyła", "PL"},
+    {"Johann Andre Forfang", "NOR"}
+]
